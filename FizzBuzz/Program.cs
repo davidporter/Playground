2ff8b0da47e94b273319cf95996aacc6c15d8d14
David Porter <davidbrianporter@gmail.com>
2018-03-22 10:24:13 -0400

    experiemting with a simple .net core program

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;   // start at 1
+            while (i < 101)  // go up to 100
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i + " FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine(i + " Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine(i + " Buzz");
+                }
+                i++;
+            }
+        }
+    }
+}
